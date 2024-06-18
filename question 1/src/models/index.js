4d0efea596b5fcca1f135d6873722bd906974b38
vibin-velumani <vibin04v@gmail.com>
2024-06-18 14:04:26 +0530

    FINAL

--- question 1/src/models/index.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-dotenv.config()
-mongoose.connect(`${process.env.DB_URL}`)
-
-export default mongoose
--- question 1/src/route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import ProductController from './controller.js'
-
-const router = express.Router()
-router.get('/test/companies/:companyname/categories/:categoryname/products', ProductController.getNProducts);
-
-
-export default router
